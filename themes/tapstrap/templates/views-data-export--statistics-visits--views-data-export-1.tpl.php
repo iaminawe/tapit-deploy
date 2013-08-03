c446df135ccc1fc50d5b15cc669492438d618e98
Gregg Coppen <gregg@iaminawe.com>
2013-08-02 22:37:07 -0700

    overriding xml template markup
    Signed-off-by: Gregg Coppen <gregg@iaminawe.com>

---
+++ themes/tapstrap/templates/views-data-export--statistics-visits--views-data-export-1.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file views-view-table.tpl.php
+ * Template to display a view as a table.
+ *
+ * - $title : The title of this group of rows.  May be empty.
+ * - $rows: An array of row items. Each row is an array of content
+ *   keyed by field ID.
+ * - $header: an array of headers(labels) for fields.
+ * - $themed_rows: a array of rows with themed fields.
+ * @ingroup views_templates
+ */
+?>
+<?php foreach ($themed_rows as $count => $row): ?>
+<?php foreach ($row as $field => $content): ?>
+ <?php print $content; ?>
+<?php endforeach; ?>
+<?php endforeach; ?>
